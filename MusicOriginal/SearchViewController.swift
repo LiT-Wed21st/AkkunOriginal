a4101d0a14991502e5a137ad53931dbf069a4c93
tsuyuni <tsuyu.lit@gmail.com>
2023-09-01 02:52:48 +0900

    SearchViewControllerのコード修正

--- MusicOriginal/SearchViewController.swift
+++ MusicOriginal/SearchViewController.swift
@@ -13,14 +13,17 @@ class SearchViewController: UIViewController {
     @IBOutlet var searchBar: UISearchBar!
     @IBOutlet var tableView: UITableView!
     
+    // 検索ワードに一致するアーティストの配列
     var artists: [Artist] = []
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        // searchBarのデザイン
         searchBar.backgroundImage = UIImage()
     }
     
+    // 検索ワードに一致するアーティストを取得する
     func searchArtist(name: String) {
         Task {
             do {
@@ -36,19 +39,24 @@ class SearchViewController: UIViewController {
     }
 }
 
+// searchBarの設定
 extension SearchViewController: UISearchBarDelegate {
+    
+    // 文字列が変更された時の処理
     func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
-        print(searchBar.text!)
-        // 入力された文字列でアーティストを検索
         searchArtist(name: searchBar.text!)
     }
 }
 
+// tableViewの設定
 extension SearchViewController: UITableViewDelegate, UITableViewDataSource {
+    
+    // セルの数
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return artists.count
     }
     
+    // セルの内容
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "cell")!
         let label = cell.viewWithTag(1) as! UILabel
@@ -56,6 +64,7 @@ extension SearchViewController: UITableViewDelegate, UITableViewDataSource {
         return cell
     }
     
+    // セルが押された際の処理
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         let artist = artists[indexPath.row]
         let nav = self.presentingViewController as! UINavigationController
