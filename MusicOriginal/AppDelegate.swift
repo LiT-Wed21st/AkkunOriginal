06ba70c0ea12f49c57f4bfe7677cc850494fae6e
tsuyuni <tsuyu.lit@gmail.com>
2023-09-04 22:06:08 +0900

    修正

--- MusicOriginal/AppDelegate.swift
+++ MusicOriginal/AppDelegate.swift
@@ -12,8 +12,6 @@ import RealmSwift
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
     
-    var window: UIWindow?
-    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         sleep(1)
         Task {
